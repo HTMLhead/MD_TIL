298143d1272fb12eeaaf2fa28d5094eaf5dff11c
HTMLhead <mrdoo309@gmail.com>
2019-11-13 22:28:46 +0900

    update(postData): post fetch메서드 구현

--- react/fe-qna-plain/public/qna.js
+++ react/fe-qna-plain/public/qna.js
@@ -3,6 +3,18 @@ const URL = {
   INIT: "http://localhost:3000/api/questions",
   LOGIN: "http://localhost:3000/api/login"
 };
+
+function postData(url = "", data = {}) {
+  return fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  }).then(response => response.json()); // parses JSON response into native JavaScript objects
+}
+
 function getQnATemplate(list) {
   return list.reduce((html, { title, question, questionId, answers }) => {
     return (
