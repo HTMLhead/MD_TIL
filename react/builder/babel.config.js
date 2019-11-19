a6da24b2e5ee1331d0c14291b6e92f2fc3a19915
HTMLhead <mrdoo309@gmail.com>
2019-11-19 20:43:21 +0900

    proposal 기능은 별도 plugin설치 및 적용

--- react/builder/babel.config.js
+++ react/builder/babel.config.js
@@ -1,17 +1,24 @@
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(true); // 바벨은 설정 파일을 계속해서 실행하고 싶지 않아함(만약 내용이 같다면) 그럴때 cache해서 사용할 수 있도록 해주는 함수
 
   const presets = [
     [
       "@babel/preset-env",
       {
-        // targets: { ie: "11" }
-        targets: "> 0.25%, not dead"
+        targets: "> 0.25%, not dead",
+        useBuiltIns: "usage",
+        corejs: "3",
+        modules: false
       }
     ]
   ];
 
+  const plugins = [
+    ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ];
+
   return {
-    presets
+    presets,
+    plugins
   };
 };
