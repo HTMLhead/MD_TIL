3f03c67fb40d1ebd94141f3dc6b37826c437b567
HTMLhead <mrdoo309@gmail.com>
2019-11-20 22:54:41 +0900

    update(app): provider적용

--- react/builder/src/scripts/App.jsx
+++ react/builder/src/scripts/App.jsx
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from "react";
-import List from "./List.jsx";
+import React from "react";
+import ContextProvider from "./ContextProvider/index.jsx";
+import NewsTask from "./NewsTask/index.jsx";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState();
-
-  const changeInputData = e => {
-    setNewTodo(e.target.value);
-  };
-
-  const addTodo = e => {
-    setTodos([...todos, newTodo]);
-  };
-
-  useEffect(() => {
-    console.log("새로운 렌더링!", todos);
-  }, [todos]);
-
   return (
-    <>
-      <h1>todo 애플리케이션</h1>
-
-      <form action="">
-        <input type="text" name="" onChange={changeInputData} />
-        <button onClick={() => addTodo()}>할일추가</button>
-      </form>
-
-      <List todos={todos} />
-    </>
+    <ContextProvider>
+      <NewsTask />
+    </ContextProvider>
   );
 };
 
