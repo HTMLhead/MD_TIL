2dff692e1d6a4d793f1d3ca6aa93d1255f314876
HTMLhead <mrdoo309@gmail.com>
2019-11-20 22:55:31 +0900

    feat(useFetch): useFetch 커스텀 훅 제작

---
+++ react/builder/src/lib/useFecth.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const useFetch = (opt, url) => {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url, opt);
+        const json = await res.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
