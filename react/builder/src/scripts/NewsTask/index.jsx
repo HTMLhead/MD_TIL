dc05ba58e58941dc874574c4c79ae2290583953d
HTMLhead <mrdoo309@gmail.com>
2019-11-21 00:02:34 +0900

    update(newsTask): 랜더링 될 때 초기화되는 기능 추가

--- react/builder/src/scripts/NewsTask/index.jsx
+++ react/builder/src/scripts/NewsTask/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useStateValue } from "../GlobalStateProvider/index.jsx";
 import useFetch from "../../lib/useFecth.js";
 
 const fetchOption = { method: "GET", mode: "cors" };
 const dataUrl =
   "https://gist.githubusercontent.com/crongro/6928f4707c55da24a27e366579c2288e/raw/c288f6ba05b883862c186afcb295bbdde20077ff/newsstand-news-json.js";
+
 const NewsTask = () => {
+  const [{ news }, dispatch] = useStateValue();
   const { data, loading, error } = useFetch(dataUrl, fetchOption);
 
-  if (loading) return loading;
-  if (error) return error;
-  console.log(data);
+  React.useEffect(() => {
+    if (loading) return;
+    dispatch({ type: "INIT_DATA", args: [...data] });
+  }, [data]);
+
+  if (loading) return <div>loading</div>;
+  if (error) return <div>error{error}</div>;
   return <div>hi</div>;
 };
 
