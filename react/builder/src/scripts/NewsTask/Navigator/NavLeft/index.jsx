cdc37c640634a45368607dacdfdb7813ea2dba56
HTMLhead <mrdoo309@gmail.com>
2019-11-25 21:35:03 +0900

    update(leftNav): nav왼쪽 디자인 완성

--- react/builder/src/scripts/NewsTask/Navigator/NavLeft/index.jsx
+++ react/builder/src/scripts/NewsTask/Navigator/NavLeft/index.jsx
@@ -4,27 +4,32 @@ import styled from "styled-components";
 import { Arrow } from "../../../../lib/icons.js";
 
 const NavLeftWrapper = styled.div`
+  width: 20rem;
+  padding: 1rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  font-size: 1.4rem;
 `;
 const NewsStand = styled.div`
   padding: 0 2px;
+  font-weight: bold;
 `;
 const WholePress = styled.div`
   padding: 0 15px;
+  color: #666;
 `;
 const Divider = styled.div`
-  border-right: 1px solid #000;
+  border-right: 1px solid #666;
   height: 15px;
 `;
 
 const NewsTask = () => {
   return (
     <NavLeftWrapper>
-      <NewsStand>뉴스 스탠드</NewsStand>
+      <NewsStand>뉴스스탠드</NewsStand>
       <Arrow />
-      <WholePress>전체언론사</WholePress>
+      <WholePress>전체 언론사</WholePress>
       <Divider />
     </NavLeftWrapper>
   );
