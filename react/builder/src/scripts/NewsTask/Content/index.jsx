ee414f9dc2dc8f58eb62964dcf44d66856d8eb7a
HTMLhead <mrdoo309@gmail.com>
2019-11-25 22:28:42 +0900

    update(content): 전체 컨텐츠 스타일 제작 완료

--- react/builder/src/scripts/NewsTask/Content/index.jsx
+++ react/builder/src/scripts/NewsTask/Content/index.jsx
@@ -5,6 +5,10 @@ import NewsContent from "./NewsContent/index.jsx";
 
 const ContentWrapper = styled.div`
   display: flex;
+  width: 65rem;
+  border-bottom: 1px solid #666;
+  border-right: 1px solid #666;
+  border-left: 1px solid #666;
 `;
 
 const Content = () => {
