c1e554c89741dfc75cdc7493f8f00ca676c53e09
HTMLhead <mrdoo309@gmail.com>
2019-11-25 21:50:31 +0900

    update(ContentPageMover): 페이지 이동 버튼 스타일 추가

--- react/builder/src/scripts/NewsTask/Navigator/NavRight/ContentPageMover/index.jsx
+++ react/builder/src/scripts/NewsTask/Navigator/NavRight/ContentPageMover/index.jsx
@@ -5,21 +5,26 @@ import { LeftArrow, RightArrow } from "styled-icons/boxicons-regular";
 const BtnWrapper = styled.div`
   display: flex;
   align-items: center;
-  width: 40px;
+  width: 8rem;
 `;
 
 const EachBtnWrapper = styled.div`
-  width: 20px;
+  padding: 1rem;
+  border-left: 1px solid #666;
+  color: #666;
+  &:hover {
+    color: #139ffb;
+  }
 `;
 
 const ConetnePageMover = () => {
   return (
     <BtnWrapper>
       <EachBtnWrapper>
-        <LeftArrow />
+        <LeftArrow style={{ width: "2rem" }} />
       </EachBtnWrapper>
       <EachBtnWrapper>
-        <RightArrow />
+        <RightArrow style={{ width: "2rem" }} />
       </EachBtnWrapper>
     </BtnWrapper>
   );
