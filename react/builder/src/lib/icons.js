f2198760b3d4fa52f8591a49fe300440b353f12b
HTMLhead <mrdoo309@gmail.com>
2019-11-25 21:32:26 +0900

    update(arrow): arrow아이콘 사이즈 수정

--- react/builder/src/lib/icons.js
+++ react/builder/src/lib/icons.js
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const ArrowIcon = styled.svg`
   width: 5px;
   height: 5px;
-  border-top: 3px solid #000;
-  border-right: 3px solid #000;
+  border-top: 2px solid #666;
+  border-right: 2px solid #666;
   border-radius: 1px;
   transform: rotate(45deg);
   margin-right: 5px;
