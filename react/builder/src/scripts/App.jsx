8752008204e46d217595138116f47f5e51fecca8
HTMLhead <mrdoo309@gmail.com>
2019-11-25 21:22:38 +0900

    update(app): 리셋 css 추가, 폰트 추가, 전체 폰트 사이즈 수정

--- react/builder/src/scripts/App.jsx
+++ react/builder/src/scripts/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import GlobalStateProvider from "./GlobalStateProvider/index.jsx";
 import CurrentStateProvider from "./CurrentStateProvider/index.jsx";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import NewsTask from "./NewsTask/index.jsx";
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html {
+    font-size: 10px;
+    font-family: 'NanumBarunGothic', sans-serif;
+  }
+`;
 const App = () => {
   return (
     <CurrentStateProvider>
       <GlobalStateProvider>
+        <GlobalStyle />
         <NewsTask />
       </GlobalStateProvider>
     </CurrentStateProvider>
