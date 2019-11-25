fc225969f6589f53e4d18cfdedfb20b31e5596ae
HTMLhead <mrdoo309@gmail.com>
2019-11-25 20:39:14 +0900

    update(current-stateReducer): 데이터 초기화, 현재 뉴스 리듀서 동작 추가

--- react/builder/src/scripts/CurrentStateProvider/stateReducer.js
+++ react/builder/src/scripts/CurrentStateProvider/stateReducer.js
@@ -1,5 +1,18 @@
 export default (state, action) => {
   switch (action.type) {
+    case "INIT_DATA":
+      return {
+        ...state,
+        ALL_DATA: action.args
+      };
+    case "CURRENT_NEWS":
+      const newsData = state.ALL_DATA.find(
+        news => news.company === action.args
+      );
+      return {
+        ...state,
+        ...newsData
+      };
     default:
       return { ...state };
   }
